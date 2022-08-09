f9e7394f9a133c7a7ebb53938c7d496bed885312
Daniel Thwaites <danthwaites30@btinternet.com>
2022-08-09 23:03:02 +0100

    Don't recreate `SinkController` for every refresh :zap:

--- src/electrodes/volume.rs
+++ src/electrodes/volume.rs
@@ -2,18 +2,24 @@ use gtk::prelude::*;
 use pulsectl::controllers::{DeviceControl, SinkController};
 use crate::{Electrode, make_icon};
 
-pub struct Volume { label: gtk::Label }
+pub struct Volume {
+    label: gtk::Label,
+    controller: SinkController
+}
 
 impl Electrode for Volume {
     fn initialize(parent: &gtk::Box) -> Self {
         let (box_, label) = make_icon(&parent, "");
         box_.style_context().add_class("electrode");
-        Volume { label }
+
+        let controller = SinkController::create().expect("could not connect to PulseAudio");
+
+        Volume { label, controller }
     }
 
     fn refresh(&mut self) {
-        let mut handler = SinkController::create().expect("could not connect to PulseAudio");
-        let device = handler.get_default_device().expect("could not get default PulseAudio device");
+        let device = self.controller.get_default_device()
+            .expect("could not get default PulseAudio device");
 
         if device.mute {
             self.label.set_label("Mute");
