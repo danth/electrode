e87ae272fd159c537da9a02f997b9a99c8240836
Daniel Thwaites <danthwaites30@btinternet.com>
2022-09-18 10:42:20 +0100

    Remove NixOS color option :bug:
    This functionality was removed in the previous commit.

--- nixos.nix
+++ nixos.nix
@@ -4,20 +4,12 @@ self:
 with lib;
 
 {
-  options.programs.electrode = {
-    enable = mkOption {
-      description = "Whether to install the Electrode status bar";
-      type = types.bool;
-      # As this module has to be installed separately we can assume
-      # that the user wants to use it by default.
-      default = true;
-    };
-
-    color = mkOption {
-      description = "Color of the status bar text";
-      type = types.str;
-      default = "#000000";
-    };
+  options.programs.electrode.enable = mkOption {
+    description = "Whether to install the Electrode status bar";
+    type = types.bool;
+    # As this module has to be installed separately we can assume
+    # that the user wants to use it by default.
+    default = true;
   };
 
   config = mkIf config.programs.electrode.enable {
@@ -29,9 +21,7 @@ with lib;
       partOf = [ "graphical-session.target" ];
       wantedBy = [ "graphical-session.target" ];
 
-      serviceConfig.ExecStart =
-        "${self.packages.${pkgs.system}.default}/bin/electrode"
-        + " --color ${config.programs.electrode.color}";
+      serviceConfig.ExecStart = "${self.packages.${pkgs.system}.default}/bin/electrode";
     };
 
     fonts.fonts = [ pkgs.font-awesome ];
