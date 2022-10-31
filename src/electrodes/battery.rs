823a55f43aead1823dfbf74597392c3d77671236
Daniel Thwaites <danthwaites30@btinternet.com>
2022-10-31 09:31:51 +0000

    Display actual battery energy :sparkles:
    A lot more can be determined from the two statistics which are now shown:
    
    - Remaining battery life (energy / power)
    - A full charge (power = 0)
    - Rate of discharge (power)
    - Energy efficiency (power)
    - Battery capacity (energy when fully charged)
    
    A percentage makes it difficult to know the battery life when it is not
    displayed, and encourages charging to 100% which increases wear on some
    types of batteries.

--- src/electrodes/battery.rs
+++ src/electrodes/battery.rs
@@ -1,24 +1,24 @@
 use async_std::task;
 use battery::Manager;
+use battery::units::energy::watt_hour;
 use battery::units::power::watt;
-use battery::units::ratio::percent;
 use gtk::prelude::*;
 use gtk::glib::{self, clone};
 use crate::electrodes::{DEFAULT_POLLING_DURATION, Electrode, make_label};
 
 fn setup_battery(parent: &gtk::Box, mut battery: battery::Battery) {
-    let percentage_label = make_label(parent);
+    let energy_label = make_label(parent);
     let power_label = make_label(parent);
 
     let manager = Manager::new().unwrap();
 
     glib::MainContext::default().spawn_local(clone!(
-        @weak percentage_label, @weak power_label =>
+        @weak energy_label, @weak power_label =>
         async move {
             loop {
-                let percentage = battery.state_of_charge().get::<percent>();
-                let percentage_text = format!("{:.0}%", percentage);
-                percentage_label.set_label(&percentage_text);
+                let energy = battery.energy().get::<watt_hour>();
+                let energy_text = format!("{:.0}Wh", energy);
+                energy_label.set_label(&energy_text);
 
                 let power = battery.energy_rate().get::<watt>();
                 let power_text = format!("{:.1}W", power);
